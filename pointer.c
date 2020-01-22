060f06d821b40c5d7a01a38b91779fa5a886c5fb
Vibhor Agarwal <agarwalvibhor99@gmail.com>
2020-01-22 16:15:29 -0500

    Initial

---
+++ pointer.c
@@ -0,0 +1,338 @@
+// DO NOT INCLUDE ANY OTHER LIBRARIES/FILES
+#include "pointer.h"
+
+// Compares the price of obj1 with obj2
+// Returns a negative number if the price of obj1 is less than the price of obj2
+// Returns a positive number if the price of obj1 is greater than the price of obj2
+// Returns 0 if the price of obj1 is equal to the price of obj2
+int compare_by_price(Object* obj1, Object* obj2)
+{
+    // IMPLEMENT THIS
+   if ((obj1->virtual_func_table.price(obj1))<(obj2->virtual_func_table.price(obj2))){
+        return -1;
+    }
+    else if((obj1->virtual_func_table.price(obj1))>(obj2->virtual_func_table.price(obj2))){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+// Compares the quantity of obj1 with obj2
+// Returns a negative number if the quantity of obj1 is less than the quantity of obj2
+// Returns a positive number if the quantity of obj1 is greater than the quantity of obj2
+// Returns 0 if the quantity of obj1 is equal to the quantity of obj2
+int compare_by_quantity(Object* obj1, Object* obj2)
+{
+    // IMPLEMENT THIS
+    if ((obj1->quantity)<(obj2->quantity)){
+        return -1;
+    }
+    else if ((obj1->quantity)>(obj2->quantity)){
+        return 1;
+    }
+    else{
+        return 0;    
+    }
+}
+
+// Initializes a StaticPriceObject with the given quantity, name, and price
+void static_price_object_construct(StaticPriceObject* obj, unsigned int quantity, const char* name, double price)
+{
+    // IMPLEMENT THIS
+    obj->obj.quantity=quantity;
+    obj->obj.name= name; 
+    obj->price= price;
+    obj->obj.virtual_func_table.price=(price_fn)static_price;
+    obj->obj.virtual_func_table.bulk_price=(bulk_price_fn)static_bulk_price;
+}
+
+// Initializes a DynamicPriceObject with the given quantity, name, base price, and price scaling factor
+void dynamic_price_object_construct(DynamicPriceObject* obj, unsigned int quantity, const char* name, double base, double factor)
+{
+    // IMPLEMENT THIS
+    obj->obj.quantity=quantity;
+    obj->obj.name= name;
+    obj->base= base;
+    obj->factor = factor;
+    obj->obj.virtual_func_table.price= (price_fn)dynamic_price;
+    obj->obj.virtual_func_table.bulk_price=(bulk_price_fn)dynamic_bulk_price;
+}
+
+// Returns the price of a StaticPriceObject or ERR_OUT_OF_STOCK if it is out of stock
+double static_price(StaticPriceObject* obj)
+{
+    // IMPLEMENT THIS
+    if(obj->obj.quantity>0){
+        return obj->price;
+    }
+    else
+        return ERR_OUT_OF_STOCK; 
+}
+
+// Returns the price of a DynamicPriceObject or ERR_OUT_OF_STOCK if it is out of stock
+// The dynamic price is calculated as the base price multiplied by (the quantity raised to the power of the scaling factor)
+double dynamic_price(DynamicPriceObject* obj)
+{
+    // IMPLEMENT THIS
+    double obj_price;
+    if(obj->obj.quantity>0){
+        obj_price = (obj->base)*pow(obj->obj.quantity, obj->factor);
+        return obj_price;
+    }
+    else
+        return ERR_OUT_OF_STOCK; 
+}
+
+// Returns the bulk price of purchasing multiple (indicated by quantity parameter) StaticPriceObject at a discount where the first item is regular price and the additional items are scaled by the BULK_DISCOUNT factor
+// Return ERR_OUT_OF_STOCK of there is insufficient quantity available
+double static_bulk_price(StaticPriceObject* obj, unsigned int quantity)
+{
+    // IMPLEMENT THIS
+    double total_bulk_price;
+    if(quantity==0)
+        return 0;
+    if(obj->obj.quantity<quantity)
+        return ERR_OUT_OF_STOCK;
+    else{
+        total_bulk_price= obj->obj.virtual_func_table.price(obj)+((quantity-1)*obj->obj.virtual_func_table.price(obj)*BULK_DISCOUNT);
+        return total_bulk_price;
+    }
+}
+
+// Returns the bulk price of purchasing multiple (indicated by quantity parameter) DynamicPriceObject at a discount where the first item is regular price and the additional items are scaled by the BULK_DISCOUNT factor
+// This uses the same dynamic price equation from the dynamic_price function, and note that the price changes for each item that is bought
+// For example, if 3 items are requested, each of them will have a different price, and this function calculates the total price of all 3 items
+// Return ERR_OUT_OF_STOCK of there is insufficient quantity available
+double dynamic_bulk_price(DynamicPriceObject* obj, unsigned int quantity)
+{
+
+    // IMPLEMENT THIS
+    double amount = 0;
+    if(quantity==0)
+        return 0;
+    if(obj->obj.quantity<quantity)
+        return ERR_OUT_OF_STOCK; 
+
+    else{
+        amount = (obj->base)*pow(obj->obj.quantity,obj->factor);
+        if(quantity == 1){
+            return amount;
+        }
+        for(int i = 1; i< quantity; i++){
+            amount = amount + (obj->base)*pow((int)obj->obj.quantity-(i),obj->factor)*BULK_DISCOUNT;
+        }
+        return amount;
+    }    
+        
+}
+
+//
+// Iterator functions
+//
+
+// Initializes an iterator to the beginning of a list
+void iterator_begin(LinkedListIterator* iter, LinkedListNode** head)
+{
+    // IMPLEMENT THIS
+    iter->prev_next = head;
+    iter->curr = *head;
+}
+
+// Updates an iterator to move to the next element in the list if possible
+void iterator_next(LinkedListIterator* iter)
+{
+    // IMPLEMENT THIS
+    if(iter->curr!=NULL){
+        iter->prev_next=&(iter->curr->next);
+        iter->curr = iter->curr->next;
+    }
+    else
+        return;
+}
+
+// Returns true if iterator is at the end of the list or false otherwise
+bool iterator_at_end(LinkedListIterator* iter)
+{
+    // IMPLEMENT THIS
+    if(iter->curr==NULL)
+        return true;
+    else 
+        return false;
+}
+
+// Returns the current object that the iterator references or NULL if the iterator is at the end of the list
+Object* iterator_get_object(LinkedListIterator* iter)
+{
+    // IMPLEMENT THIS
+    if(iter->curr!=NULL){
+        return iter->curr->obj;
+    }
+    else
+        return NULL;
+}
+
+// Removes the current node referenced by the iterator
+// The iterator is valid after call and references the next object
+// Returns removed node
+LinkedListNode* iterator_remove(LinkedListIterator* iter)
+{
+    // IMPLEMENT THIS
+    LinkedListNode *removed_node;
+    removed_node = iter->curr;
+    if(iter->curr == NULL){
+     return NULL;
+    }
+    else{
+        *iter->prev_next = iter->curr->next;
+        iter->curr=iter->curr->next;
+        return removed_node;
+    }
+}
+
+// Inserts node after the current node referenced by the iterator
+// The iterator is valid after call and references the same object as before
+// Returns ERR_INSERT_AFTER_END error if iterator at the end of the list or 0 otherwise
+int iterator_insert_after(LinkedListIterator* iter, LinkedListNode* node)
+{
+    // IMPLEMENT THIS
+    if(iter->curr == NULL){
+        return ERR_INSERT_AFTER_END;
+    }
+    else{
+        node->next = iter->curr->next;
+        iter->curr->next = node; //
+        return 0;
+    }
+}
+
+// Inserts node before the current node referenced by the iterator
+// The iterator is valid after call and references the same object as before
+void iterator_insert_before(LinkedListIterator* iter, LinkedListNode* node)
+{
+    // IMPLEMENT THIS
+        node->next = iter->curr;
+        *iter->prev_next = node;
+        iter->prev_next = &(node -> next);
+}
+
+//
+// List functions
+//
+
+// Returns the maximum, minimum, and average price of the linked list
+void max_min_avg_price(LinkedListNode** head, double* max, double* min, double* avg)
+{
+    // IMPLEMENT THIS
+    LinkedListIterator iter ;
+    iterator_begin(&iter,head);
+    *max= object_price(iterator_get_object(&iter));
+    *min= object_price(iterator_get_object(&iter));
+    while(!iterator_at_end(&iter)){
+        *avg = *avg + object_price(iterator_get_object(&iter));
+        if(*max<object_price(iterator_get_object(&iter))){
+            *max = object_price(iterator_get_object(&iter));
+        } 
+        if(*min>object_price(iterator_get_object(&iter))){
+            *min = object_price(iterator_get_object(&iter));
+        }
+        iterator_next(&iter);
+    }
+    *avg = *avg/length(head);
+}
+
+// Executes the func function for each node in the list
+// The function takes in an input data and returns an output data, which is used as input to the next call to the function
+// The initial input data is provided as a parameter to foreach, and foreach returns the final output data
+// For example, if there are three nodes, foreach should behave like: return func(node3, func(node2, func(node1, data)))
+Data foreach(LinkedListNode** head, foreach_fn func, Data data)
+{
+    // IMPLEMENT This
+    LinkedListIterator iter ;
+    iterator_begin(&iter,head);
+    while(!iterator_at_end(&iter)){
+        data = func(iterator_get_object(&iter),data);
+        iterator_next(&iter);
+    }
+
+    return data;
+}
+
+// Returns the length of the list
+int length(LinkedListNode** head)
+{
+    // IMPLEMENT THIS
+    int len = 0;
+    LinkedListIterator iter ;
+    iterator_begin(&iter,head);
+    while(!iterator_at_end(&iter)){
+        len = len+1;
+        iterator_next(&iter);
+    }
+    return len;
+}
+
+//
+// Mergesort
+//
+
+// Assuming list1 and list2 are sorted lists, merge list2 into list1 while keeping it sorted
+// The sort order is determined by the compare function: compare returns the difference
+void merge(LinkedListNode** list1_head, LinkedListNode** list2_head, compare_fn compare)
+{
+    // IMPLEMENT THIS
+    LinkedListIterator iter1, iter2;
+    iterator_begin(&iter1,list1_head);
+    iterator_begin(&iter2,list2_head);
+    while(!iterator_at_end(&iter1) && !iterator_at_end(&iter2)){
+        if(compare(iterator_get_object(&iter1), iterator_get_object(&iter2))<0){
+            if(iter1.curr->next == NULL){
+                while(!iterator_at_end(&iter2)){
+                    LinkedListNode* removed_node = NULL;
+                    removed_node = iterator_remove(&iter2);
+                    iterator_insert_after(&iter1,removed_node);
+                    iterator_next(&iter1);
+                }
+            }
+            else{         
+                iterator_next(&iter1);
+            }
+        }
+        else if(compare(iterator_get_object(&iter1), iterator_get_object(&iter2))>0){
+            LinkedListNode* removed_node = NULL;
+            removed_node = iterator_remove(&iter2);
+            iterator_insert_before(&iter1, removed_node);
+        }
+    }
+}
+
+// Split the list head in half and place half in the split list
+// For example, if head has 8 nodes, then split will move 4 of them to split_head
+void split(LinkedListNode** head, LinkedListNode** split_head)
+{
+    
+    // IMPLEMENT THIS
+    LinkedListIterator iter ;
+    iterator_begin(&iter,head);
+    int half_length = length(head)/2;
+    for (int i =1; i < half_length; i++){
+        iterator_next(&iter);
+    }
+    *split_head = iter.curr->next;
+    iter.curr->next= NULL;
+}
+
+// Implement the mergesort algorithm to sort the list
+// The sort order is determined by the compare function
+void mergesort(LinkedListNode** head, compare_fn compare)
+{
+    // IMPLEMENT THIS
+    LinkedListNode* split_head = NULL;
+    if(length(head)==0 || length(head)==1)
+        return;
+    split(head, &split_head);
+    mergesort(head, compare);
+    mergesort(&split_head, compare);
+    merge(head, &split_head, compare);
+}
